e6f24074d91a34c9e697a9b5b613fd1980e57beb
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2021-06-30 11:45:58 +0200

    Added checksum computation

--- build_lg2.sh
+++ build_lg2.sh
@@ -98,4 +98,8 @@ done
 framework=lg2
 rm -rf $framework.xcframework
 xcodebuild -create-xcframework -framework build/Release-iphoneos/$framework.framework -framework build/Release-iphonesimulator/$framework.framework -output $framework.xcframework
+# while we're at it, let's compute the checksum:
+rm -f $framework.xcframework.zip
+zip -rq $framework.xcframework.zip $framework.xcframework
+swift package compute-checksum $framework.xcframework.zip
 
