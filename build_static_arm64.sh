7739873b73e83ab9db8f1064602907d270b2a88b
Emma Labbé <emma@labbe.me>
2025-02-08 11:55:48 -0300

    Add build_static_arm64.sh

---
+++ build_static_arm64.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# M4 Required with Xcode beta:
+export M4=$(xcrun -f m4)
+IOS_SDKROOT=$(xcrun --sdk iphoneos --show-sdk-path)
+
+source_dir=$PWD
+
+mkdir -p build-iphoneos
+pushd build-iphoneos
+cmake $source_dir \
+    -DREGEX_BACKEND=builtin -DBUILD_CLAR=OFF -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=OFF -DTHREADSAFE=OFF\
+    -DBUILD_SHARED_LIBS=OFF \
+    -DCMAKE_INSTALL_PREFIX=@rpath \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_OSX_SYSROOT=${IOS_SDKROOT} \
+    -DCMAKE_C_COMPILER=$(xcrun --sdk iphoneos -f clang) \
+    -DCMAKE_C_FLAGS="-arch arm64 -target arm64-apple-darwin19.6.0 -O2 -miphoneos-version-min=14 -I${source_dir}" \
+    -DCMAKE_MODULE_LINKER_FLAGS="-arch arm64 -target arm64-apple-darwin19.6.0 -O2 -miphoneos-version-min=14" \
+    -DCMAKE_SHARED_LINKER_FLAGS="-arch arm64 -target arm64-apple-darwin19.6.0 -O2 -miphoneos-version-min=14" \
+    -DCMAKE_EXE_LINKER_FLAGS="-arch arm64 -target arm64-apple-darwin19.6.0 -O2 -miphoneos-version-min=14" \
+    -DCMAKE_LIBRARY_PATH=${IOS_SDKROOT}/lib/ \
+    -DCMAKE_INCLUDE_PATH=${IOS_SDKROOT}/include/
+make
