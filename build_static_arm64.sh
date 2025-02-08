4cb5d3b7a2d7a1f0818075d54757bba98a7dec54
Emma Labbé <emma@labbe.me>
2025-02-08 17:14:21 -0300

    Use libssh2 headers from ../libssh2/libssh2/libssh2

--- build_static_arm64.sh
+++ build_static_arm64.sh
@@ -4,12 +4,14 @@
 export M4=$(xcrun -f m4)
 IOS_SDKROOT=$(xcrun --sdk iphoneos --show-sdk-path)
 
+export LIBSSH2_INCLUDE_DIR=../libssh2/libssh2/libssh2
+
 source_dir=$PWD
 
 mkdir -p build-iphoneos
 pushd build-iphoneos
 cmake $source_dir \
-    -DREGEX_BACKEND=builtin -DBUILD_CLAR=OFF -DBUILD_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=OFF -DTHREADSAFE=OFF\
+    -DREGEX_BACKEND=builtin -DBUILD_CLAR=OFF -DBUILD_TESTS=OFF -DUSE_SSH=ON -DBUILD_EXAMPLES=OFF -DBUILD_SHARED_LIBS=OFF -DTHREADSAFE=OFF\
     -DBUILD_SHARED_LIBS=OFF \
     -DCMAKE_INSTALL_PREFIX=@rpath \
     -DCMAKE_BUILD_TYPE=Release \
@@ -20,5 +22,5 @@ cmake $source_dir \
     -DCMAKE_SHARED_LINKER_FLAGS="-arch arm64 -target arm64-apple-darwin19.6.0 -O2 -miphoneos-version-min=14" \
     -DCMAKE_EXE_LINKER_FLAGS="-arch arm64 -target arm64-apple-darwin19.6.0 -O2 -miphoneos-version-min=14" \
     -DCMAKE_LIBRARY_PATH=${IOS_SDKROOT}/lib/ \
-    -DCMAKE_INCLUDE_PATH=${IOS_SDKROOT}/include/
+    -DCMAKE_INCLUDE_PATH="${LIBSSH2_INCLUDE_DIR};${IOS_SDKROOT}/include/" \
 make
